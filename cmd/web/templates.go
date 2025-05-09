e6b6c19527e20d257213270b51f0ad3ebac019bf
OlehD88 <demchukoleg88@gmail.com>
2025-05-09 20:36:31 +0200

    Add middlewares

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,50 @@
 package main
 
-import "github.com-OlehD88-snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+	"time"
+
+	"github.com-OlehD88-snippetbox/internal/models"
+)
 
 type templateData struct {
-	Snippet  models.Snippet
-	Snippets []models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+		// Parse the files into a template set.
+		ts, err := template.New(name).Funcs(functions).ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
